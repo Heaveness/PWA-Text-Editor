5a99c3bb1c71bf02a460ecb95c87c2fc0319f917
Heaveness <vindictuslbp@gmail.com>
2023-07-27 19:58:32 -0400

    Added and fixed some files where the html files were not found in the right directories. As well as add code into install.js to install button for Chrome Dev.

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,6 +3,24 @@ import Editor from './editor';
 import './database';
 import '../css/style.css';
 
+const main = document.querySelector('#main');
+main.innerHTML = '';
+
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
+
 const editor = new Editor();
 
 // Check if service workers are supported
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  // Show the install button
+  butInstall.style.display = 'block';
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  // Show the installation prompt
+  deferredPrompt.prompt();
+  const { outcome } = await deferredPrompt.userChoice;
+  if (outcome === 'accepted') {
+    console.log('User accepted the PWA installation');
+  } else {
+    console.log('User declined the PWA installation');
+  }
+  // Hide the install button
+  butInstall.style.display = 'none';
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Hide the install button if the app is already installed
+window.addEventListener('appinstalled', () => {
+  butInstall.style.display = 'none';
+});
